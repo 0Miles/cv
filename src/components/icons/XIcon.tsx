7255f87456ed42112939f188bbcc3208935dbff0
Miles <git@miles.tw>
2024-01-04 19:20:54 +0800

    Update files and remove unused logos

--- src/components/icons/XIcon.tsx
+++ src/components/icons/XIcon.tsx
@@ -1,11 +1,11 @@
 export const XIcon = (props: React.SVGProps<SVGSVGElement>) => {
   return (
-    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <title>X</title>
-      <path
-        fill="currentColor"
-        d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
-      />
-    </svg>
+      <svg xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+          stroke-linejoin="round" {...props}>
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
+          <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
+      </svg>
   );
 };
--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,277 @@
-import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-} from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+import { DiscordIcon } from "@/components/icons/DiscordIcon";
+import { UserIcon } from "lucide-react";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
-  about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
-  summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
-  contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
-    social: [
-      {
-        name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
-        icon: GitHubIcon,
-      },
-      {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
-        icon: LinkedInIcon,
-      },
-      {
-        name: "X",
-        url: "https://x.com/BartoszJarocki",
-        icon: XIcon,
-      },
+    name: "Miles Huang",
+    initials: "MH",
+    location: "Taipei, Taiwan, TST",
+    locationLink: "https://www.google.com/maps/place/Taipei",
+    about:
+        "Detail-oriented Full Stack Engineer dedicated to crafting high-quality products.",
+    summary:
+        "I am a full-stack engineer who values attention to detail and always keeps the end user in mind. I prioritize aspects such as performance, accessibility, UI/UX, maintainability, and scalability in my work. I have over 3 years of experience working remotely with an overseas company.",
+    avatarUrl: "https://avatars.githubusercontent.com/u/8224584?v=4",
+    personalWebsiteUrl: "https://miles.tw",
+    contact: {
+        email: "inbox@miles.tw",
+        // tel: "+48530213401",
+        social: [
+            {
+                name: "Personal website",
+                url: "https://miles.tw",
+                icon: UserIcon,
+            },
+            {
+                name: "GitHub",
+                url: "https://github.com/0Miles",
+                icon: GitHubIcon,
+            },
+            {
+                name: "Discord",
+                url: "https://discordapp.com/users/347738300909617152",
+                icon: DiscordIcon,
+            },
+            {
+                name: "X",
+                url: "https://twitter.com/milestw",
+                icon: XIcon,
+            },
+        ],
+    },
+    education: [
+        {
+            school: "National Taiwan University of Science and Technology",
+            degree: "Bachelor of Business Administration",
+            start: "2014",
+            end: "2019",
+        },
     ],
-  },
-  education: [
-    {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
-    },
-  ],
-  work: [
-    {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
-      description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
-    },
-  ],
-  skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
-  ],
-  projects: [
-    {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
+    work: [
+        {
+            company: "Master.co",
+            link: "https://css.master.co/",
+            badges: ["Volunteer"],
+            title: "Full Stack Developer",
+            start: "2022",
+            end: "Now",
+            description:
+                "Master CSS is an atomic CSS framework whose syntax closely resembles native CSS. My involvement includes a small portion of the syntax, some website development work, and creating related utility programs(e.g., VS Code IntelliSense, ESLint plugin).",
+        },
+        {
+            company: "Everyware Limited",
+            link: "https://www.everyware.com.hk/",
+            badges: ["Remote"],
+            title: "Full Stack Developer",
+            start: "2018",
+            end: "2023",
+            description:
+                "Everyware is a restaurant systems software company. I'm responsible for developing new POS system features, fixing software errors, and improving functionalities in the customer management backend.",
+        },
+        {
+            company: "Aoyue Design",
+            link: "https://www.facebook.com/AoyueDesign/",
+            badges: [],
+            title: "Full Stack Developer",
+            start: "2018",
+            end: "2023",
+            description:
+                "AoyueDesign is a web development outsourcing company.",
+        }
+    ],
+    skills: [
+        "JavaScript",
         "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
-      link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
-      },
-    },
-    {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
+        "React / Next.js",
+        "Angular",
         "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
-  ],
+        "C#",
+        ".Net",
+    ],
+    projects: [
+        {
+            title: "Soku Source Editor",
+            techStack: [
+                "Side Project",
+                "React",
+                "Master CSS",
+                "Electron"
+            ],
+            description: "Used for editing the mod source for the SokuLauncher mod store, managing and publishing mods on the source.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/soku-source-editor",
+            },
+        },
+        {
+            title: "Easy Party",
+            techStack: ["Side Project", "Next.js", "Firebase", "Master CSS", "Primitives"],
+            description:
+                "It's a website that figures out when people are free for events. Handy for planning stuff.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/easy-party",
+            },
+        },
+        {
+            title: "Master CSS ESlint Plugin",
+            techStack: ["Typescript", "Next.js"],
+            description:
+                "An official ESLint plugin for enforcing team coding styles, making your template markup more organized, and catching syntax errors early.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/master-css-eslint-plugin",
+            },
+        },
+        {
+            title: "Soku Launcher",
+            techStack: ["Side Project", "C#", "WPF", ".Net Framework"],
+            description:
+                "Launcher for Touhou 12.3: Hisoutensoku. Configuring mods(SWRSToys) before launching the game and provide automatic mod updates and download missing mods.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/soku-launcher",
+            },
+        },
+        {
+            title: "Master CSS Language Service",
+            techStack: ["TypeScript", "Node.js"],
+            description:
+                "Enhanced Master CSS development experience, including syntax highlighting, code completion, syntax rule preview, and more.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/master-css-language-service",
+            },
+        },
+        {
+            title: "Master CSS Playground",
+            techStack: ["Next.js", "Master CSS", "Firebase"],
+            description:
+                "Enhanced Master CSS development experience, including syntax highlighting, code completion, syntax rule preview, and more.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/master-css-playground",
+            },
+        },
+        {
+            title: "Wenhsiung",
+            techStack: ["Angular", "NestJS", "Master UI"],
+            description:
+                "Wenhsiung glasses online shopping website.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/wenhsiung",
+            },
+        },
+        {
+            title: "Love Home",
+            techStack: ["Angular", "NestJS", "Master UI"],
+            description:
+                "Love Home restaurant online ordering APP(PWA).",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/love-home",
+            },
+        },
+        {
+            title: "CLCC Booking Dashboard",
+            techStack: ["Angular", "Firebase", "Bootstrap"],
+            description:
+                "CLCC Management Interface for Homepage Information, Room Types, Customer Booking, and More.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/clcc",
+            },
+        },
+        {
+            title: "Everyware CRM",
+            techStack: ["Angular", "Firebase", "Master UI"],
+            description:
+                "Everyware’s Customer Relationship Management System.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/everyware-crm",
+            },
+        },
+        {
+            title: "Everyware CRM",
+            techStack: ["Angular", "Firebase", "Master UI"],
+            description:
+                "Everyware’s Customer Relationship Management System.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/everyware-crm",
+            },
+        },
+        {
+            title: "NBC",
+            techStack: ["Angular", "Master UI"],
+            description:
+                "NBC official website.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/nbc",
+            },
+        },
+        {
+            title: "FE Payment Machine",
+            techStack: ["HTML", "Master UI"],
+            description:
+                "FE payment machine page.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/fe-payment-machine",
+            },
+        },
+        {
+            title: "Secret Wardrobe",
+            techStack: ["Angular", "Master UI"],
+            description:
+                "Secret Wardrobe online shopping website.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/secret-wardrobe",
+            },
+        },
+        {
+            title: "Spe3D",
+            techStack: ["HTML", "Sass"],
+            description:
+                "Spe3D official mobile website.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/spe3d",
+            },
+        },
+        {
+            title: "DogeBeats",
+            techStack: ["Visual Basic .NET", "DxLibrary"],
+            description:
+                "Doge meme themed rhythm game.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/dogebetas",
+            },
+        },
+        {
+            title: "Pada Order",
+            techStack: ["Flask", "HTML", "CSS"],
+            description:
+                "NTUST school restaurant online ordering APP.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/pada",
+            },
+        },
+        {
+            title: "CLVSC",
+            techStack: ["HTML", "CSS", "JQuery"],
+            description:
+                "Taoyuan Municipal Zhong Li Commercial Senior High School official website.",
+            link: {
+                label: "Detail",
+                href: "https://miles.tw/works/clvsc",
+            },
+        },
+    ],
 } as const;
